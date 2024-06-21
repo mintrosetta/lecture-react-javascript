5299e055a6a8c85e968cabe1aa51da2dd6e33c88
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-21 15:00:04 +0700

    initial project

---
+++ reactpro/src/App.jsx
@@ -0,0 +1,16 @@
+import Hello from "./components/Hello";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Hello />
+        <Hello />
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+// https://www.youtube.com/watch?v=SV0fSOIKAZQ&list=PLSsAz5wf2lkK_ekd0J__44KG6QoXetZza&index=9
---
+++ reactpro/src/components/Hello.jsx
@@ -0,0 +1,15 @@
+const name = "mint";
+
+function displayMsg() {
+    return "Mint.Rosetta";
+}
+
+function Hello() {
+  return (
+    <>
+      <h1>Hello from a component {displayMsg()}</h1>
+    </>
+  );
+}
+
+export default Hello;
