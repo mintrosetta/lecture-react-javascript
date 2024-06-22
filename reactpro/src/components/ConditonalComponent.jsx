5abce9d5913afdd50b89dfd89931c06b04459821
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-22 11:54:03 +0700

    ternary condition

--- reactpro/src/components/ConditonalComponent.jsx
+++ reactpro/src/components/ConditonalComponent.jsx
@@ -4,13 +4,7 @@ import Code from './Code';
 export default function ConditionalComponent() {
     const display = false;
 
-    let message = null;
-
-    if (display) {
-        message = <h1>This is message 1</h1>;
-    } else {
-        message = <h1>This is message 2</h1>;
-    }
+    let message = (display) ? <Welcome /> : <Code />;
 
     return message;
 }
