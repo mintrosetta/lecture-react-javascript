041cbcddd562050ca93da86448ac087602ff4f83
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-22 11:12:49 +0700

    render array of object

--- reactpro/src/components/Fruits.jsx
+++ reactpro/src/components/Fruits.jsx
@@ -1,12 +1,18 @@
 export default function Fruits() {
-  const fruits = ["Apple", "Mango", "Banana", "Orange"];
+  // const fruits = ["Apple", "Mango", "Banana", "Orange"];
+  const fruits = [
+    { name: "Apple", price: 10 },
+    { name: "Banana", price: 2 },
+    { name: "Mango", price: 4 },
+    { name: "Orange", price: 1.2 }
+  ];
 
   return (
     <>
       <div>
         <ul>
           {fruits.map((fruit) => {
-            return <li key={fruit}>{fruit}</li>;
+            return <li key={fruit.name}>{fruit.name} price: {fruit.price}</li>;
           })}
         </ul>
       </div>
