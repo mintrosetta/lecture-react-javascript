18b01cdee2b09cdc0779c80e21c533f682504102
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-22 12:08:51 +0700

    condition for render

--- reactpro/src/components/Fruit.jsx
+++ reactpro/src/components/Fruit.jsx
@@ -1,8 +1,12 @@
 export default function Fruit(props) {
-  const { name, price } = props; 
-  return (
-    <>
-      <li>{name} {price}</li>
-    </>
-  );
+    const { name, price, count } = props;
+    return (
+        <>
+            {/* <li>{name} {price}</li> */}
+            {/* we don't need to show price less than 5, return empty string */}
+            <li>
+                {name} {price} - { count == 0 ? "soldout" : "" }
+            </li>
+        </>
+    );
 }
--- reactpro/src/components/Fruits.jsx
+++ reactpro/src/components/Fruits.jsx
@@ -2,19 +2,20 @@ import Fruit from "./Fruit";
 
 export default function Fruits() {
   const fruits = [
-    { name: "Apple", price: 10 },
-    { name: "Banana", price: 2 },
-    { name: "Mango", price: 4 },
-    { name: "Orange", price: 1.2 },
+    { name: "Apple", price: 10, count: 0 },
+    { name: "Banana", price: 2, count: 2 },
+    { name: "Mango", price: 6, count: 0 },
+    { name: "Orange", price: 1.2, count: 2 },
   ];
 
   return (
+    // <></> is a fragment, not render like element
     <>
       <div>
         <ul>
           {fruits.map((fruit) => {
             return (
-              <Fruit key={fruit.name} name={fruit.name} price={fruit.price} />
+              <Fruit key={fruit.name} name={fruit.name} price={fruit.price} count={fruit.count} />
             );
           })}
         </ul>
