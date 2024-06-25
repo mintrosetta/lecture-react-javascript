40e9efebd5002cb513f4f1e9d7ab12da9c76dd75
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-25 15:27:06 +0700

    multiple state

--- reactpro/src/components/Counter.jsx
+++ reactpro/src/components/Counter.jsx
@@ -4,13 +4,23 @@ export default function Counter() {
     // variable, function for update state of variable 
     // useState({default value})
     const [count, setCount] = useState(0);
+    const [incrementBy, setIncrementBy] = useState(1);
+    const [decreaseBy, setDecreaseBy] = useState(1);
 
     function incrementHandlerClick() {
-        setCount(count + 1);
+        setCount(count + incrementBy);
     }
 
     function decrementHandlerClick() {
-        setCount(count - 1);
+        setCount(count - decreaseBy);
+    }
+
+    function increaseIncrementHandlerClick() {
+        setIncrementBy(incrementBy + 1);
+    }
+
+    function decreaseIncrementHandlerClick() {
+        setDecreaseBy(decreaseBy + 1);
     }
 
     return (
@@ -19,6 +29,10 @@ export default function Counter() {
                 <h1>Count value is: {count}</h1>
                 <button onClick={incrementHandlerClick}>Increment</button>
                 <button onClick={decrementHandlerClick}>Decrement</button>
+            
+                <h1>We are incrementing the value by: {incrementBy}</h1>
+                <button onClick={increaseIncrementHandlerClick}>Increase Increment</button>
+                <button onClick={decreaseIncrementHandlerClick}>Decrease Increment</button>
             </div>
         </>
     );
