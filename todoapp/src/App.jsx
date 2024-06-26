2d18738614a8c79e4b168e4d402ec4f2677f71e7
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-27 00:07:09 +0700

    inline css

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Todo from "./components/Todo";
+import InlineComponent from "./components/InlineComponent";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <>
-            <Todo />
+            <InlineComponent />
         </>
     );
 }
---
+++ todoapp/src/components/InlineComponent.jsx
@@ -0,0 +1,9 @@
+const heanderStyle = { color: "blue", fontSize: "150px" };
+
+export default function InlineComponent() {
+    return (
+        <>
+            <p style={heanderStyle}>Inline Component</p>
+        </>
+    );
+}
