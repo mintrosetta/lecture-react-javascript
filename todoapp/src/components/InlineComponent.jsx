de61ce05010213174ccc370a7fc6f2abd13ac675
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-28 09:58:28 +0700

    css stylesheet to component

--- todoapp/src/components/InlineComponent.jsx
+++ todoapp/src/components/InlineComponent.jsx
@@ -1,9 +1,12 @@
+import '../css/style.css';
+
 const heanderStyle = { color: "blue", fontSize: "150px" };
 
 export default function InlineComponent() {
     return (
         <>
-            <p style={heanderStyle}>Inline Component</p>
+            {/* <p style={heanderStyle}>Inline Component</p> */}
+            <h1 className='header'>Inline Component</h1>
         </>
     );
 }
