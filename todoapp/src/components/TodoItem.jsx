7dd6c18ce4aa30083a624176fe3a9602bdafde0d
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-29 18:07:47 +0700

    remove function

--- todoapp/src/components/TodoItem.jsx
+++ todoapp/src/components/TodoItem.jsx
@@ -1,7 +1,15 @@
 import styles from './TodoItem.module.css';
 
 export default function TodoItem(props) {
-    const { item } = props;
+    const { item, todos, setTodos } = props;
+
+    function deleteHandler(item) {
+        console.log("Delete " +  item);
+        let newTodos = todos.filter((todo) => {
+            return todo !== item;
+        });
+        setTodos(newTodos);
+    }
 
     return (
         <>
@@ -9,7 +17,7 @@ export default function TodoItem(props) {
                 <div className={styles.itemName}>
                     {item}
                     <span>
-                        <button className={styles.deleteBtn}>x</button>
+                        <button onClick={() => deleteHandler(item)} className={styles.deleteBtn}>x</button>
                     </span>
                 </div>
                 <hr className={styles.line} />
--- todoapp/src/components/TodoList.jsx
+++ todoapp/src/components/TodoList.jsx
@@ -1,12 +1,12 @@
 import TodoItem from './TodoItem';
 import styles from './TodoList.module.css';
 
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, setTodos }) {
     return (
         <>
             <div className={styles.list}>
                 {todos.map((item) => (
-                    <TodoItem key={item} item={item}/>
+                    <TodoItem key={item} item={item} setTodos={setTodos} todos={todos}/>
                 ))}
             </div>
         </>
