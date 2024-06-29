ac2250ca94b8e62398d06cd923b7778599bb2c6e
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-29 13:14:03 +0700

    add delete btn

--- todoapp/src/components/TodoItem.jsx
+++ todoapp/src/components/TodoItem.jsx
@@ -6,7 +6,12 @@ export default function TodoItem(props) {
     return (
         <>
             <div className={styles.item}>
-                <div className={styles.itemName}>{item}</div>
+                <div className={styles.itemName}>
+                    {item}
+                    <span>
+                        <button className={styles.deleteBtn}>x</button>
+                    </span>
+                </div>
                 <hr className={styles.line} />
             </div>
         </>
