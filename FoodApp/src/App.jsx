13b4a21092489e1f4dcd4a58078f2172d87a33d7
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-30 13:38:54 +0700

    create nav component

--- FoodApp/src/App.jsx
+++ FoodApp/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Search from "./components/Search";
 import FoodList from "./components/FoodList";
+import Nav from "./components/Nav";
 
 function App() {
   const [foods, setFoods] = useState([]);
 
   return (
     <>
+      <Nav />
       <Search foods={foods} setFoods={setFoods} />
       <FoodList foods={foods} />
     </>
