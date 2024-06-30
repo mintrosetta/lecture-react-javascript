7cf96a7fdfab45511a77666bddf43928564d8c72
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-30 13:07:08 +0700

    call api

--- FoodApp/src/components/Search.jsx
+++ FoodApp/src/components/Search.jsx
@@ -1,17 +1,20 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
+const URL = 'https://api.spoonacular.com/recipes/complexSearch';
+const API_KEY = '342567d3ea164139a18fac0b0d51b2e0';
+
 export default function Search() {
     const [search, setSearch] = useState("");
 
     // syntax of use effect hook
     // มันจะถูกเรียกใช้งาน 2 ครั้งเพราะ react อยู่ในโหมดเข้มงวด ถ้าปิดมันจะทำงานแค่ 1 ครั้ง (<React.StrictMode>)
     useEffect(() => {
-        function demo() {
-            console.log("Hello World");
+        async function fetchFood() {
+            const response = await (await fetch(`${URL}?apiKey=${API_KEY}&query=${search}`)).json();
+            console.log(response);
         }
-
-        demo();
+        fetchFood();
     }, [search]);
 
     return (
