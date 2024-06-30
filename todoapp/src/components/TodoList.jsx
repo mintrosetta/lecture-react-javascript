885306cb53541d0f84841730f579fe85eea125b1
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-06-30 10:57:04 +0700

    sorting todo

--- todoapp/src/components/TodoList.jsx
+++ todoapp/src/components/TodoList.jsx
@@ -2,10 +2,11 @@ import TodoItem from './TodoItem';
 import styles from './TodoList.module.css';
 
 export default function TodoList({ todos, setTodos }) {
+    const sortedTodos = todos.slice().sort((a, b) => a.done - b.done);
     return (
         <>
             <div className={styles.list}>
-                {todos.map((item) => (
+                {sortedTodos.map((item) => (
                     <TodoItem key={item.name} item={item} setTodos={setTodos} todos={todos}/>
                 ))}
             </div>
